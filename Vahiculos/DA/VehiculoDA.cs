5330ca53d0b4febbde30fdf2a0fa94a66118f4b5
Laboratorios <Laboratorios@R17>
2025-07-16 20:36:31 -0600

    Codigo

---
+++ Vahiculos/DA/VehiculoDA.cs
@@ -0,0 +1,98 @@
+﻿using Abstracciones.Interfaces.DA;
+using Abstracciones.Modelos;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Drawing;
+
+namespace DA
+{
+    public class VehiculoDA : IVehiculoDA
+    {
+        private IRepositorioDapper _repositorioDapper;
+        private SqlConnection _sqlConnection;
+
+        #region Constructor
+
+        public VehiculoDA(IRepositorioDapper repositorioDapper)
+        {
+            _repositorioDapper = repositorioDapper;
+            _sqlConnection = _repositorioDapper.ObtenerRepositorio();
+        }
+        #endregion
+
+        #region Operaciones
+        public async Task<Guid> Agregar(VehiculoRequest vehiculo)
+        {
+            string query = @"AgregarVehiculo";
+            var resultadoConsulta = await _sqlConnection.ExecuteScalarAsync<Guid>(query, new
+            {
+                Id = Guid.NewGuid(),
+                IdModelo = vehiculo.IdModelo,
+                Placa = vehiculo.Placa,
+                Color = vehiculo.Color,
+                Anio = vehiculo.Anio,
+                Precio = vehiculo.Precio,
+                CorreoPropietario = vehiculo.CorreoPropietario,
+                TelefonoPropietario = vehiculo.TelefonoPropietario
+            });
+            return resultadoConsulta;
+        }
+
+        public async Task<Guid> Editar(Guid Id, VehiculoRequest vehiculo)
+        {
+            await VerificarVehiculoExiste(Id);
+            string query = @"EditarVehiculo";
+            var resultadoConsulta = await _sqlConnection.ExecuteScalarAsync<Guid>(query, new
+            {
+                Id = Id,
+                IdModelo = vehiculo.IdModelo,
+                Placa = vehiculo.Placa,
+                Color = vehiculo.Color,
+                Anio = vehiculo.Anio,
+                Precio = vehiculo.Precio,
+                CorreoPropietario = vehiculo.CorreoPropietario,
+                TelefonoPropietario = vehiculo.TelefonoPropietario
+            });
+            return resultadoConsulta;
+        }
+
+        public async Task<Guid> Eliminar(Guid id)
+        {
+            await VerificarVehiculoExiste(id);
+            string query = @"EliminarVehiculo";
+            var resultadoConsulta = await _sqlConnection.ExecuteScalarAsync<Guid>(query, new
+            {
+                Id = id
+            });
+            return resultadoConsulta;
+        }
+
+        public async Task<IEnumerable<VehiculoResponse>> Obtener()
+        {
+            string query = @"ObtenerVehiculos";
+            var resultadoConsulta = await _sqlConnection.QueryAsync<VehiculoResponse>(query);
+            return resultadoConsulta;
+        }
+
+        public async Task<VehiculoDetalle> Obtener(Guid id)
+        {
+            string query = @"ObtenerVehiculo";
+            var resultadoConsulta = await _sqlConnection.QueryAsync<VehiculoDetalle>(query, new {
+                Id = id
+            });
+            return resultadoConsulta.FirstOrDefault();
+        }
+        #endregion
+
+        #region Helpers
+        private async Task VerificarVehiculoExiste(Guid Id)
+        {
+            VehiculoResponse? resultadoConsultaVehiculo = await Obtener(Id);
+            if (resultadoConsultaVehiculo == null)
+            {
+                throw new Exception("No se encontró el vehiculo");
+            }
+        }
+        #endregion
+    }
+}
